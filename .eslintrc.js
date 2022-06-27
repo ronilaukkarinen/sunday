0ac25a39b74b931641bf3ce23cdeea53936afcf9
Roni Laukkarinen <roni@dude.fi>
2022-06-27 20:54:51 +0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/**
+ * @Author: Roni Laukkarinen
+ * @Date:   2021-11-18 15:12:33
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2021-11-22 18:38:05
+ */
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  rules: {
+    indent: ['error', 2],
+  },
+  env: {
+    browser: true,
+    jquery: true,
+  },
+};
